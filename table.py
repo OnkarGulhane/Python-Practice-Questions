85ae920e8a29f1817ba13dfc7096998e9e47e68d
Onkar Gulhane <omkar288113@gmail.com>
2025-08-22 18:38:06 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,14 @@
+#while loop
+
+# a = int(input('Enter a number:'))
+# i = 1
+# while i<=10:
+#     print(a,'*',i,'=',a*i)
+#     i+=1
+
+#for loop
+
+a = int(input('Enter a number:'))
+for i in range(1,11):
+    print(a,'*',i,'=',a*i)
+print('Value of I is :',i)    
